e13f39314139bf84e8a4d7fb9283dc5dce253589
kristina03g <kkristina03g@yandex.ru>
2023-03-30 00:06:44 +0400

    created models for sequelize

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const express = require('express');
+const Sequelize = require("sequelize");
 
+const sequelize = new Sequelize("hvostiki_db", "root", "", {
+  dialect: "mysql",
+  host: "localhost"
+});
